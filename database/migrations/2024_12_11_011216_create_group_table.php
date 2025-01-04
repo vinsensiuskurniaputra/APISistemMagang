75aaefd9ec32f0ab036118fb7aa16ef3cc4eb6e4
vinsensiuskurniaputra <vinsensiuskurniaputra01@gmail.com>
2025-01-04 20:13:43 +0700

    edit migration file

--- database/migrations/2024_12_11_011216_create_group_table.php
+++ database/migrations/2024_12_11_011216_create_group_table.php
@@ -12,26 +12,23 @@ return new class extends Migration
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('lecturer_id');
+            $table->foreignId('lecturer_id')->constrained('lecturers')->cascadeOnDelete();
             $table->string('title');
             $table->string('icon')->nullable();
             $table->string('color')->nullable();
             $table->timestamps();
-
-            $table->foreign('lecturer_id')->references('id')->on('lecturers')->onDelete('cascade');
         });
 
-        Schema::create('group_student', function (Blueprint $table) {
+        Schema::cchroreate('group_student', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('group_id');
-            $table->unsignedBigInteger('student_id');
+            $table->foreignId('group_id')->constrained('groups')->cascadeOnDelete();
+            $table->foreignId('student_id')->constrained('students')->cascadeOnDelete();
             $table->timestamps();
 
-            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
-            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
             $table->unique(['group_id', 'student_id']);
         });
     }
+
 
     /**
      * Reverse the migrations.
